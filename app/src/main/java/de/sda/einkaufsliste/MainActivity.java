b143b0ede1a029e3d2af7f99a07d0a8f4fc938e8
Dmitry Sokolyuk <sokolyuk@yahoo.com>
2016-07-06 14:22:29 +0200

    -

--- app/src/main/java/de/sda/einkaufsliste/MainActivity.java
+++ app/src/main/java/de/sda/einkaufsliste/MainActivity.java
@@ -2,8 +2,13 @@ package de.sda.einkaufsliste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,18 +25,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        ((Button)findViewById(R.id.btnAddShopping)).setOnClickListener(
-                v->{
-                    EditText res = (EditText)findViewById(R.id.txtShoppingList);
-                    String pre = "";
-                    if (!res.getText().toString().isEmpty()) pre = "\n";
-                    res.append(
-                            pre + ((EditText)findViewById(R.id.txtProduct)).getText().toString() + "/" + ((EditText)findViewById(R.id.txtShop)).getText().toString()
-                    );
-                }
-        );*/
+        MainActivityOnClickListner l = new MainActivityOnClickListner(this);
 
-        ((Button)findViewById(R.id.btnAddShopping)).setOnClickListener(new MainActivityOnClickListner(this));
+        ((Button)findViewById(R.id.btnAddShopping)).setOnClickListener(l);
 
+/*        ((Button)findViewById(R.id.btnClear)).setOnClickListener(e->{
+            shoppingList.clear();
+            ((EditText)findViewById(R.id.txtShoppingList)).setText("");
+            showMess("Cleared");
+        });*/
+
+        ((Button)findViewById(R.id.btnClearShopping)).setOnClickListener(l);
+
+        ((Button)findViewById(R.id.btnSaveShopping)).setOnClickListener(l);
+
+        //SharedPreferences a;
+
+    }
+
+    public void showMess(String mess) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup)findViewById(R.id.toast_layout_root));
+        TextView text = (TextView)layout.findViewById(R.id.text);
+        text.setText(mess);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.RIGHT | Gravity.TOP, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }
--- app/src/main/java/de/sda/einkaufsliste/controller/MainActivityOnClickListner.java
+++ app/src/main/java/de/sda/einkaufsliste/controller/MainActivityOnClickListner.java
@@ -1,12 +1,13 @@
 package de.sda.einkaufsliste.controller;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import de.sda.einkaufsliste.MainActivity;
 import de.sda.einkaufsliste.R;
@@ -31,8 +32,59 @@ public class MainActivityOnClickListner implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+
+        switch(v.getId()) {
+            case R.id.btnAddShopping:
+                addShopping();
+                break;
+            case R.id.btnClearShopping:
+                clearShopping();
+                break;
+            case R.id.btnSaveShopping:
+                saveShopping();
+                break;
+            default:
+                mainActivity.showMess("Unexpected caller. Oops.");
+        }
+    }
+
+    private void saveShopping() {
+        try{
+            try(FileOutputStream fos = mainActivity.openFileOutput("shoppings.txt", MainActivity.MODE_ENABLE_WRITE_AHEAD_LOGGING);) {
+
+                fos.write(MainActivity.shoppingList.toString().getBytes());
+                fos.flush();
+            }
+            mainActivity.showMess("Saved.");
+
+            try(FileInputStream fi = mainActivity.openFileInput("shoppings.txt");) {
+                byte[] b = new byte[100];
+                String res = "";
+                int n = 0;
+                while ((n = fi.read(b)) != -1) {
+                    res += new String(b, 0, n);
+                }
+                mainActivity.showMess(res);
+                System.out.println(res);
+            }
+
+        }catch(Exception e){
+            mainActivity.showMess("Error occured: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void clearShopping() {
+        MainActivity.shoppingList.clear();
+        txtShoppingList.setText("");
+        mainActivity.showMess("Cleared");
+    }
+
+    protected void addShopping() {
         String productName = txtProduct.getEditableText().toString();
         String shopName = txtShop.getEditableText().toString();
+
+        Toast toast;
 
         if (!productName.isEmpty() && !shopName.isEmpty()) {
             Shopping shopping = new Shopping(productName, shopName);
@@ -47,10 +99,13 @@ public class MainActivityOnClickListner implements View.OnClickListener {
 
             txtProduct.setText("");
             txtShop.setText("");
-            Toast.makeText(mainActivity, "Done", Toast.LENGTH_SHORT);
+            toast = Toast.makeText(mainActivity.getApplicationContext(), "Done", Toast.LENGTH_SHORT);
             Log.i("Inf", "Done.");
         } else {
-            Toast.makeText(mainActivity, "Bitte alle Felder ausfuellen", Toast.LENGTH_LONG);
+            toast = Toast.makeText(mainActivity.getApplicationContext(), "Bitte alle Felder ausfuellen", Toast.LENGTH_LONG);
         }
+        toast.setGravity(Gravity.TOP| Gravity.CENTER, 10, 0);
+        toast.show();
     }
+
 }
